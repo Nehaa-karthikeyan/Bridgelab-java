a246329b0e387a4aa6186050423a6e7ec985530f
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-01-28 15:35:07 +0530

    [Nehaa] : [Update] : Made changes in the collections folder.

---
+++ src/javacollectionsandstreams/collections/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+package javacollectionsandstreams.collections;
+
+import java.time.LocalDate;
+import java.util.*;
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(3, 1, 2, 2, 3, 4);
+        Set<Integer> set = new LinkedHashSet<>(list);
+
+        System.out.println(new ArrayList<>(set));
+    }
+}
